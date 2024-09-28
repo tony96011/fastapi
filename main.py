d6ee43581e41078b115f94cd5dcdc75b35304961
109062171 <tonyhsu96011@gmail.com>
2024-09-28 15:27:21 +0800

    homework2

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+from enum import Enum
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item, q: str = None):
+    result = {"item_id": item_id, **item.model_dump()}
+    if q:
+        result.update({"q": q})
+    return result
